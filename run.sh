07b3a23f379a8189cfac3bac7ce89592ede35ae7
Denny Vettom <dennysv@gmail.com>
2024-03-27 16:13:07 +0000

    Update by DV on Wed 27 Mar 2024 16:13:07 GMT

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+cd ~/MyRepos/demohelmrepo
+helm lint charts/dv-demo-app
+if [ $? -ne 0 ]
+then
+	echo "ERROR: Helm lint failed"
+	exit 1
+fi
 helm package charts/dv-demo-app
 helm repo index --url https://vettom.github.io/demohelmrepo/ --merge index.yaml .
 git config user.email dennysv@gmail.com; git add -A . ; git commit -m "Update by DV on `date` " ; git push
